6ea6a7e27583cfe7423ddbe7457deccf343ea9cf
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-04-22 09:17:19 -0300

    FEAT: Create API for Acessorio

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from core.views.acessorio import AcessorioViewSet
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -7,11 +8,12 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet
+from core.views import UserViewSet, AcessorioViewSet
 
 router = DefaultRouter()
 
 router.register(r'usuarios', UserViewSet, basename='usuarios')
+router.register(r'acessorios', AcessorioViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
